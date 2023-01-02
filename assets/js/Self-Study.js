5550ac5cd83aa3e851ff14ef7cf5a00a7073bcf1
Omer Isik <isikomer4545@gmail.com>
2023-01-02 19:05:06 +0100

    first

--- assets/js/Self-Study.js
+++ assets/js/Self-Study.js
@@ -24,3 +24,23 @@ const time = new Date();
 
     document.getElementById("digitalClock").innerHTML = currentTime;
     }, 1000)
+
+    document.getElementById("calculatorBtn").addEventListener("click", ()=>{
+        let operator = document.getElementById("operatorCalc").value;
+        let num1 = Number(document.getElementById("num1Calc").value);
+        let num2 = Number(document.getElementById("num2Calc").value);
+        switch(operator){
+            case "+" : 
+            document.getElementById("answerCalc").innerHTML = num1+num2;
+            break;
+            case "-" : 
+            document.getElementById("answerCalc").innerHTML = num1-num2;
+            break;
+            case "*" : 
+            document.getElementById("answerCalc").innerHTML = num1*num2;
+            break;
+            case "/" : 
+            document.getElementById("answerCalc").innerHTML = Math.(num1/num2);
+            break;
+        }
+    })
